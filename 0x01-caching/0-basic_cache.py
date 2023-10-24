a2c860d604369baea7cf57d593a2727a12a5efef
sentense1 <3xsaint@gmail.com>
2023-10-24 19:16:47 +0000

    seperated base_caching from basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,36 +2,7 @@
 """ BaseCaching module
 """
 from typing import Dict, Any
-
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self) -> None:
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item) -> None:
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")  # noqa
-
-    def get(self, key) -> None:
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")  # noqa
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
