51aba6f26a64df8495ae53b0e90077453d2aed2b
sentense1 <3xsaint@gmail.com>
2023-10-25 17:20:51 +0000

    task 5

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+""" LFU Caching """
+from base_caching import BaseCaching
+from typing import Optional, Any, List, Dict
+
+
+class LFUCache(BaseCaching):
+    """ LFU caching """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.queue = []
+        self.counter = {}
+
+    def put(self, key: Any, item: Any) -> None:
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        item_count = self.counter.get(key, None)
+
+        if item_count is not None:
+            self.counter[key] += 1
+        else:
+            self.counter[key] = 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                del self.counter[first]
+                print("DISCARD: {}".format(first))
+
+        if key not in self.queue:
+            self.queue.insert(0, key)
+        self.mv_right_list(key)
+
+    def get(self, key: Any) -> Optional[Dict]:
+        """ Gets item from cache """
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.counter[key] += 1
+            self.mv_right_list(key)
+        return item
+
+    def mv_right_list(self, item):
+        """ Moves element to the right, taking into account LFU """
+        length = len(self.queue)
+
+        idx = self.queue.index(item)
+        item_count = self.counter[item]
+
+        for i in range(idx, length):
+            if i != (length - 1):
+                nxt = self.queue[i + 1]
+                nxt_count = self.counter[nxt]
+
+                if nxt_count > item_count:
+                    break
+
+        self.queue.insert(i + 1, item)
+        self.queue.remove(item)
+
+    @staticmethod
+    def get_first_list(array: Any) -> Optional[Any]:
+        """ Get first element of list or None """
+        return array[0] if array else None
+
+
+if __name__ == "__main__":
+    my_cache = LFUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
+    my_cache.put("L", "L")
+    my_cache.print_cache()
+    my_cache.put("M", "M")
+    my_cache.print_cache()
