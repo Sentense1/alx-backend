59d11773f3730c9ee81de67cbb0074ff541f9745
Johnson Oragui <3xsaint@gmail.com>
2023-10-26 13:09:31 +0100

    updated for conciseness

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -28,7 +28,7 @@ class LFUCache(BaseCaching):
             self.counter[key] = 1
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.get_first_list(self.queue)
+            first = self.queue[0] if self.queue else None
             if first:
                 self.queue.pop(0)
                 del self.cache_data[first]
@@ -37,17 +37,17 @@ class LFUCache(BaseCaching):
 
         if key not in self.queue:
             self.queue.insert(0, key)
-        self.mv_right_list(key)
+        self.mv_right(key)
 
     def get(self, key: Any) -> Optional[Dict]:
         """ Gets item from cache """
         item = self.cache_data.get(key, None)
         if item is not None:
             self.counter[key] += 1
-            self.mv_right_list(key)
+            self.mv_right(key)
         return item
 
-    def mv_right_list(self, item):
+    def mv_right(self, item):
         """ Moves element to the right, taking into account LFU """
         length = len(self.queue)
 
@@ -64,11 +64,6 @@ class LFUCache(BaseCaching):
 
         self.queue.insert(i + 1, item)
         self.queue.remove(item)
-
-    @staticmethod
-    def get_first_list(array: Any) -> Optional[Any]:
-        """ Get first element of list or None """
-        return array[0] if array else None
 
 
 if __name__ == "__main__":
