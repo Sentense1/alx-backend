a80273f047236962052b8130ca02714a812b37d6
Johnson Oragui <3xsaint@gmail.com>
2023-10-26 11:36:39 +0100

    Update 3-lru_cache.py to concise the code

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,22 +5,22 @@ from typing import Dict, Optional, Any
 
 
 class LRUCache(BaseCaching):
-    """ LRU caching """
+    """ LRU caching class """
 
     def __init__(self):
-        """ Constructor """
+        """LRU Constructor """
         super().__init__()
         self.queue = []
 
     def put(self, key: Any, item: Any) -> None:
-        """ Puts item in cache """
+        """ Caches data in LRUCache """
         if key is None or item is None:
             return
 
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.get_first_list(self.queue)
+            first = self.queue[0] if self.queue else None
             if first:
                 self.queue.pop(0)
                 del self.cache_data[first]
@@ -32,25 +32,17 @@ class LRUCache(BaseCaching):
             self.refactor_queue(key)
 
     def get(self, key: Any) -> Optional[Dict]:
-        """ Gets item from cache """
+        """ Retrieves item from cache """
         item = self.cache_data.get(key)
         if item is not None:
             self.refactor_queue(key)
         return item
 
-    def refactor_queue(self, item: Any) -> None:
-        """ Moves element to last idx of list """
-        length = len(self.queue)
-        if self.queue[length - 1] != item:
-            self.queue.remove(item)
-            self.queue.append(item)
-
-    @staticmethod
-    def get_first_list(array: Any) -> Optional[Any]:
-        """
-        Get first element of list or None if the list is empty
-        """
-        return array[0] if array else None
+    def refactor_queue(self, key: Any) -> None:
+        """ Moves element to last index of list """
+        if self.queue[-1] != key:
+            self.queue.remove(key)
+            self.queue.append(key)
 
 
 if __name__ == "__main__":
